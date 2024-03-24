c88442861dc673d7ff5c36019f0daaf170a457ef
Mohamedsaad27 <112446463+Mohamedsaad27@users.noreply.github.com>
2024-03-25 00:34:32 +0200

    second

--- resources/views/layouts/orderHeader.blade.php
+++ resources/views/layouts/orderHeader.blade.php
@@ -11,12 +11,13 @@
     {{--bootstrap--}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
+        <script src="https://cdn.tailwindcss.com"></script>
     <!-- Scripts -->
-{{--    <link href="resources/css/style.css" >--}}
     @vite(['resources/css/style.css'])
+    @livewireStyles
 </head>
 <body class="font-sans antialiased">
+@livewireScripts
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/seller/order.blade.php
+++ resources/views/seller/order.blade.php
@@ -1,17 +1,18 @@
-@extends('layouts.adminApp')
+@extends('layouts.orderHeader')
 @section('title', 'Seller|Order')
-<div class="col-3">
 
-                @include('components.Seller.seller-vertical-nav')
+<div class="col-3">
+    @include('components.Seller.seller-vertical-nav')
 </div>
+
 <div class="container-fluid bg-secondary-dark">
     <div class="container-fluid">
         <div class="card bg-primary-dark">
             <div class="card-body pb-5 pt-3">
                 <hr>
-
+                <!-- Include the order-table component here -->
+               <livewire:order-table />
             </div>
         </div>
     </div>
 </div>
-
